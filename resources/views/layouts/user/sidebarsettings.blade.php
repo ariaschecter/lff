b21af3d6139e5f138c406f94c4760035ea2e3a6d
Your Name <you@example.com>
2022-09-06 20:30:26 +0700

    ++

--- resources/views/layouts/user/sidebarsettings.blade.php
+++ resources/views/layouts/user/sidebarsettings.blade.php
@@ -14,12 +14,12 @@
 
         <ul class="list-unstyled components">
             <img class="rounded-circle mb-5" src="{{ asset('storage/'. $user->user_picture) }}"  width="100" height="100" style="margin-left:30%;" alt="{{ $user->name }}">
-            <li><a class="nav-item" href="{{ url('progress') }}">Progress</a></li>
-            <li><a class="nav-item" href="{{ url('course') }}">Course</a></li>
-            <li><a class="nav-item" href="{{ url('order') }}">Order</a></li>
-            <li><a class="nav-item" href="{{ url('payment') }}">Payment</a></li>
+            <li><a class="nav-item" href="{{ url('progress') }}"><i class="fas fa-chart-line"></i><span class="pl-10"> Progress</span></a></li>
+            <li><a class="nav-item" href="{{ url('course') }}"><i class="fas fa-laptop-code"></i><span class="pl-10">Course</span></a></li>
+            <li><a class="nav-item" href="{{ url('order') }}"><i class="fa fa-shopping-cart"></i><span class="pl-10">Order</span></a></li>
+            <li><a class="nav-item" href="{{ url('payment') }}"><i class="fa fa-credit-card"></i><span class="pl-10">Payment</span></a></li>
             <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Setting</a>
+                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fas fa-cogs"></i><span class="pl-10">Setting</span></a>
                 <ul class="collapse list-unstyled" id="pageSubmenu">
                     <li>
                         <a href="{{ url('edit-profile') }}">Edit Profile</a>
